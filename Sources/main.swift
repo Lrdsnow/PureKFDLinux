3ed809627dc485a3c0ccbd9013d673062e64cb0e
Lrdsnow <63671193+Lrdsnow@users.noreply.github.com>
2024-10-18 13:55:36 -0700

    colors

--- Sources/main.swift
+++ Sources/main.swift
@@ -7,24 +7,31 @@ var repoHandler = RepoHandler()
 
 var repos: [Repo] = []
 
-print("Getting Repos...")
+// Bash Colors
+let green = "\u{001B}[0;32m"
+let cyan = "\u{001B}[0;36m"
+let red = "\u{001B}[0;31m"
+let reg = "\u{001B}[0;0m"
+//
+
+print("\(green)Getting Repos...\(reg)")
 for url in repoHandler.repo_urls {
     do {
         let repo = try await repoHandler.getRepoAsync(url)
         repos.append(repo)
-        print("Got Repo: \(repo.name)")
+        print("\(cyan)Got Repo: \(repo.name)\(reg)")
     } catch {
-        print(error)
+        print("\(red)\(error)\(reg)")
     }
 }
 
 var packages = repos.flatMap({ $0.packages })
 
-print("Search For Tweaks: ")
+print("\(green)Search For Tweaks: \(reg)")
 if let search = readLine() {
     if let tweak = packages.first(where: { $0.name.lowercased().contains(search.lowercased()) }) {
-        print("Found Tweak:\nName: \(tweak.name)\nAuthor: \(tweak.author ?? "Unknown")\nVersion: v\(tweak.version ?? "0")\nDescription: \(tweak.description ?? "No Description")")
+        print("\(green)Found Tweak:\nName: \(tweak.name)\nAuthor: \(tweak.author ?? "Unknown")\nVersion: v\(tweak.version ?? "0")\nDescription: \(tweak.description ?? "No Description")\(reg)")
     } else {
-        print("Tweak Not Found.")
+        print("\(red)Tweak Not Found.\(reg)")
     }
 }
